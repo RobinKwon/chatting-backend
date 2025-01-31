8c39ba8cd81594f65d636817e8b23d8f5e1ea4c5
rovin <rovin_kr@naver.com>
2025-01-31 20:28:31 +0900

    adding message db

--- app/src/models/UserStorage.js
+++ app/src/models/UserStorage.js
@@ -22,6 +22,46 @@ class UserStorage {
       });
     });
   }
+
+  static async newsession(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO chat_sessions (conversation_id, user_id, model_name) VALUES(?, ?, ?);";
+      db.query(query, [UUID(), userInfo.id, 'gpt-4o-mini'], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true });
+      });
+    });
+  }
+
+  static async savemessage(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "INSERT INTO chat_messages (conversation_id, user_id, question_or_answer, message) VALUES(?, ?, ?, ?);";
+      db.query(query, [userInfo.conversation_id, userInfo.id, userInfo.q_a, userInfo.message], (err) => {
+        if (err) reject(`${err}`);
+        else resolve({ success: true });
+      });
+    });
+  }
+
+  static async search_uid(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM chat_messages WHERE user_id = ? ORDER BY date_time ASC;";
+      db.query(query, [userInfo.id], (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data[0]);
+      });
+    });
+  }
+
+  static async search_cid(userInfo) {
+    return new Promise((resolve, reject) => {
+      const query = "SELECT * FROM chat_messages WHERE conversation_id = ? ORDER BY date_time ASC;";
+      db.query(query, [userInfoid], (err, data) => {
+        if (err) reject(`${err}`);
+        else resolve(data[0]);
+      });
+    });
+  }
 }
 
 module.exports = UserStorage;
